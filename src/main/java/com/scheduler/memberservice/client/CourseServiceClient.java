c24100b3512b2fc787fa5411016331b35fcf964c
devhong96 <dev.hong96@gmail.com>
2025-02-16 19:07:44 +0900

    feat : feign-setting

--- src/main/java/com/scheduler/memberservice/client/CourseServiceClient.java
+++ src/main/java/com/scheduler/memberservice/client/CourseServiceClient.java
@@ -1,27 +1,33 @@
 package com.scheduler.memberservice.client;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 import static com.scheduler.memberservice.client.dto.FeignCourseResponse.StudentCourseResponse;
 
-@FeignClient(name = "course-service", configuration = CourseFeignErrorDecoder.class)
+@FeignClient(
+        name = "scheduler-course-service",
+        path = "/scheduler-member-service/feign-course/",
+        configuration = CourseFeignErrorDecoder.class
+)
 public interface CourseServiceClient {
 
-    @DeleteMapping("{studentId}")
-    void deleteScheduleByStudentId(@PathVariable String studentId);
-
-    @GetMapping("{teacherId}")
-    List<StudentCourseResponse> getWeeklyCoursesByTeacherId(@PathVariable String teacherId);
-
-    @GetMapping("{studentId}")
+    @Operation(summary = "학생의 주간 수업 조회")
+    @GetMapping("/student/{studentId}/courses")
     StudentCourseResponse getWeeklyCoursesByStudentId(@PathVariable String studentId);
 
-    @PatchMapping("{teacherId}/{studentId}")
+    @Operation(summary = "학생의 스케줄 삭제")
+    @DeleteMapping("/student/{studentId}")
+    void deleteScheduleByStudentId(@PathVariable String studentId);
+
+    @Operation(summary = "선생님이 담당하는 학생들의 주간 수업 조회")
+    @GetMapping("/teacher/{teacherId}/courses")
+    List<StudentCourseResponse> getWeeklyCoursesByTeacherId(@PathVariable String teacherId);
+
+    @Operation(summary = "학생의 코스를 다른 선생님에게 재배정")
+    @PatchMapping("/teacher/{teacherId}/student/{studentId}")
     void reassignStudentCourses(@PathVariable String teacherId, @PathVariable String studentId);
 }
