ca4b05363c6b866c2dc64013d32b91bf95eaaec0
devhong96 <dev.hong96@gmail.com>
2025-08-21 03:22:14 +0900

    test: test

--- src/main/java/com/scheduler/memberservice/infra/security/SecurityConfig.java
+++ src/main/java/com/scheduler/memberservice/infra/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     }
 
     public static final String[] INTERNAL_ENDPOINTS = {
-            "/feign-member/**"
+
     };
 
     public static final String[] ADMIN_RESTRICTED_ENDPOINTS = {
@@ -63,6 +63,7 @@ public class SecurityConfig {
 
     public static final String[] ENDPOINTS_WHITELISTS = {
             "/member-api/**",
+            "/feign-member/**",
             "/actuator/**",
             "/login",
             "/help/*",
