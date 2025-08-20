1060be1a4c51ae6ae3e4aa277e5ad0979a5b6fa3
devhong96 <dev.hong96@gmail.com>
2025-08-21 02:33:41 +0900

    modify: SecurityConfig

--- src/main/java/com/scheduler/memberservice/infra/config/swagger/SwaggerConfig.java
+++ src/main/java/com/scheduler/memberservice/infra/config/swagger/SwaggerConfig.java
@@ -1,16 +1,20 @@
 package com.scheduler.memberservice.infra.config.swagger;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "Member Service Api")
-)
-
 @Configuration
-@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Member Service Api").version("1.0"))
+                .addServersItem(new Server().url("/scheduler-member-service").description("API Gateway URL"))
+                .addServersItem(new Server().url("/").description("Local DEV Server"));
+    }
 
 }
